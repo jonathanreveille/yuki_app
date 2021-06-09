86f48c77ab6bb4a6325cea8fd4620917a7e84795
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-06-09 12:00:09 +0200

    installed sentry and imports

--- settings/base.py
+++ settings/base.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 import os
 from dotenv import load_dotenv
-
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
 
@@ -144,3 +145,17 @@ AUTH_USER_MODEL = 'users.User'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL  = 'login'
 LOGIN_REDIRECT_URL = 'home'
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY"),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
