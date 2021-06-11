ef17f10a02a5801191d78086800e01f42083d5a1
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-06-11 12:09:12 +0200

    smalls correction in html templates

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Role(models.Model):
     """table to define the role
-    of the user"""
+    of the user : owner or catsitter"""
 
     name = models.CharField(max_length=20)
 
@@ -18,13 +18,18 @@ class User(AbstractUser):
     # ajouter de nouveau champs au modèle (en supposant qu'on aime bien le USER MODEL de base)
 
     email = models.EmailField('user email', max_length=255, unique=True)
-    role = models.ForeignKey('users.Role',
-                            on_delete=models.CASCADE,
-                            related_name="role",
-                            null=True, blank=True)
-
     location = models.CharField(max_length=30, blank=True)
     avatar = models.ImageField(upload_to="avatars/", default='avatar_profile.jpg')
+
+    role = models.ForeignKey('users.Role',
+                        on_delete=models.CASCADE,
+                        related_name="role",
+                        null=True, blank=True)
+                        
+    # TO IMPLEMENT CATSITTERS
+    #host_capacity = models.IntegerField(max_length=10, null=True, blank=True)
+    #following = models..??
+    #followers = models...??
 
     def __str__(self):
         return f'{self.username}'
