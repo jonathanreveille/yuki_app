aba8dddac437fe02048c28e3383b226f5ae6ab25
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-06-11 10:37:30 +0200

    healthbook and Medication CRUD made, with templates, urls and views

--- healthbook/forms.py
+++ healthbook/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import CheckboxSelectMultiple
 
 from animals.models import Pet
-from .models import HealthBook
+from .models import HealthBook, Medication
 
 
 class CustomModelMultipleChoiceField(forms.ModelMultipleChoiceField):
@@ -38,3 +38,30 @@ class CreateHealthBookForPet(forms.ModelForm):
         queryset=None,
         widget=CheckboxSelectMultiple
         )
+
+
+class CreateMedicationForPet(forms.ModelForm):
+    """method to create a custom form
+    to create a schedule for a cat
+    The idea; is that the user can only see
+    his objects"""
+
+    def __init__(self, *args, **kwargs):
+        """ Grants access to the request object so that only members
+        of the current user are given as options"""
+
+        self.request = kwargs.pop('request')
+        super(CreateMedicationForPet, self).__init__(*args, **kwargs)
+        
+        self.fields['pet'].queryset = Pet.objects.filter(
+            owner=self.request.user)
+
+    class Meta:
+
+        model = Medication
+        fields = ('pet', 'med_name',
+                'med_start', 'med_end','time','dosage')
+
+        pet = CustomModelMultipleChoiceField(
+        queryset=None,
+        widget=CheckboxSelectMultiple)
--- healthbook/urls.py
+++ healthbook/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import HealthBookList, HealthBookCreate
+from .views import HealthBookCreateView, HealthBookListView, HealthBookUpdateView, MedicationDeleteView, MedicationListView, MedicationCreateView, MedicationUpdateView
 
 app_name = "healthbook"
 
 urlpatterns = [
-    path('healthbook_list/', HealthBookList.as_view(), name="healthbook_list"),
-    path('healthbook_create/', HealthBookCreate.as_view(), name="healthbook_create"),
+    #HeathBook
+    path('healthbook_list/', HealthBookListView.as_view(), name="healthbook_list"),
+    path('healthbook_create/', HealthBookCreateView.as_view(), name="healthbook_create"),
+    path('healthbook_update/<int:pk>', HealthBookUpdateView.as_view(), name="healthbook_update"),
+    
+    #Medication
+    path('medication_list/', MedicationListView.as_view(),name="medication_list"),
+    path('medication_create/', MedicationCreateView.as_view(), name="medication_create"),
+    path('medication_update/<int:pk>', MedicationUpdateView.as_view(), name="medication_update"),
+    path('medication_delete/<int:pk>', MedicationDeleteView.as_view(), name="medication_delete"),
 ]
