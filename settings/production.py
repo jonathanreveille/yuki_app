e6f19c6c3ddd42f31d3827bfc3ee656ead0118ba
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-09-24 20:08:33 +0200

    updated allowed host

--- settings/production.py
+++ settings/production.py
@@ -4,15 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
 DEBUG = False
-ALLOWED_HOSTS = ['161.35.87.89', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['161.35.87.89']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("NAME_DB"),
-        'HOST':'localhost',
+        'HOST': 'localhost',
         'PORT': 5432,
         'USER': os.getenv("USER_DB"),
         'PASSWORD': os.getenv("PASS_DB")
