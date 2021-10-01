9f70b4c38c4cb9123fb6c68f7f37a6e03eb722b4
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-10-01 21:01:33 +0200

    edited settings in statifiles dirs

--- settings/base.py
+++ settings/base.py
@@ -152,7 +152,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/dist') # add ../dist
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
