a6ea225911e8f693db021d9eb10be0003cd940fa
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-11-23 22:57:59 +0100

    erased comments, added empty lines between imports and code

--- src/animals/admin.py
+++ src/animals/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Specie, Pet
 
-# Register your models here.
+
 admin.site.register(Specie)
 admin.site.register(Pet)
--- src/healthbook/admin.py
+++ src/healthbook/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
+
 from .models import HealthBook, Medication
+
 
 # Register your models here.
 admin.site.register(HealthBook)
