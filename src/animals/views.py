edfdaba312d189687a61b19aea363058255b1f52
jon.rev93001@gmail.com <jon.rev93001@gmail.com>
2021-11-28 16:54:42 +0100

    deleted unused import

--- src/animals/views.py
+++ src/animals/views.py
@@ -1,4 +1,3 @@
-from django.db.models import query_utils
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
